da28e5adc09f9f3237de240bca03b630c96b5616
nydia-subur <165659692+nydia-subur@users.noreply.github.com>
2024-04-30 03:07:55 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>hello!</p>
       </header>
     </div>
   );
