c9879c254e9575b557d0809250f7b9b0bdc4d2e0
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-04-30 17:49:49 +0700

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [currentSearch, setCurrentSearch] = useState("");
+  let key = `tc84aadoa14ab4fdf5f3430525cff939`;
+  // API documentation: https://www.shecodes.io/learn/apis/dictionary
+  let url = `https://api.shecodes.io/dictionary/v1/define?word=${currentSearch}&key=${key}`;
   function handleSubmit(event) {
     event.preventDefault();
+
+    axios.get(url).then(displaySearchResult);
     alert("searching");
   }
   function updateSearch(event) {
@@ -12,6 +18,10 @@ export default function Dictionary() {
     console.log(currentSearch);
   }
 
+  function displaySearchResult(response) {
+    console.log(`api result; ${response.data.word}`);
+    console.log(url);
+  }
   return (
     <div className="Dictionary">
       <form onSubmit={handleSubmit}>
