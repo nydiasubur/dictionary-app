5380e981bcd9ca0bdff307b2c2b1037d57040f94
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-05-01 15:41:17 +0800

    debugged display of Meaning.js function

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 export default function Meanings(props) {
+  function beautifySynonyms(synonyms) {
+    if (synonyms !== null) {
+      return (
+        <div>
+          <div>Synonyms</div>
+          {synonyms.join("  ")}
+        </div>
+      );
+    }
+  }
+  function displayExample(example) {
+    if (example !== null) {
+      return <span>"{example}"</span>;
+    }
+  }
   return (
     <div className="Meanings">
       <div className="partOfSpeech">{props.meaning.partOfSpeech}</div>
       <div className="definition">{props.meaning.definition}</div>
-      <div className="example">{props.meaning.example}</div>
-      <div className="synonyms">{props.meaning.synonyms}</div>
+      <div className="example">{displayExample(props.meaning.example)}</div>
+      <div className="synonyms">{beautifySynonyms(props.meaning.synonyms)}</div>
     </div>
   );
 }
